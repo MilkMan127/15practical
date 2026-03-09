8f32f2084323eed3efbd33149420feb788da3026
MilkMan127 <4394348@myuwc.ac.za>
2026-03-09 15:06:22 +0200

    Implement main method for anagram grouping
    Added main method to read words from a file and group anagrams.

--- Anagrams.java
+++ Anagrams.java
@@ -9,3 +9,31 @@ public class Anagrams {
         Arrays.sort(letters);
         return new String(letters);
     }
+     public static void main(String[] args) throws Exception {
+
+
+        BufferedReader reader = new BufferedReader(new FileReader("ulysses.txt"));;
+
+        HashMap<String, List<String>> anagrams = new HashMap<>();
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+
+            String[] words = line.split("\\s+");
+
+            for (String word : words) {
+
+
+                word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+                if (word.length() == 0)
+                    continue;
+
+                String sig = signature(word);
+
+                anagrams.putIfAbsent(sig, new ArrayList<>());
+                anagrams.get(sig).add(word);
+            }
+        }
+
