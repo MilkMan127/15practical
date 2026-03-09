d24ab9d455caebaafd08ad7ebe0a0c7fd3410cba
MilkMan127 <4394348@myuwc.ac.za>
2026-03-09 15:05:10 +0200

    Add signature method to generate word signatures

--- Anagrams.java
+++ Anagrams.java
@@ -1,1 +1,11 @@
+import java.io.*;
+import java.util.*;
 
+public class Anagrams {
+
+    
+    public static String signature(String word) {
+        char[] letters = word.toCharArray();
+        Arrays.sort(letters);
+        return new String(letters);
+    }
